b17401b198646b247f518616659d72c84f702abf
Hamidullah02 <kazihamidullah1@gmail.com>
2024-09-08 17:58:08 +0600

    first commit

---
+++ app/src/main/java/com/learning/recipeapp/Category.kt
@@ -0,0 +1,14 @@
+package com.learning.recipeapp
+
+data class Category(
+    val idCategory:String,
+    val strcategory:String,
+    val strCategoryThumb:String,
+    val strCategoryDescription:String
+    )
+data class categoriesredpons(val categories: List<Category>)
+
+
+
+
+
